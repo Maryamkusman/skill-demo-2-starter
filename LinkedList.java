561d0112164d85ba97c5946624defeff93d371c8
Maryamkusman <93306676+Maryamkusman@users.noreply.github.com>
2022-03-04 09:43:28 -0800

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -27,20 +27,20 @@ public class LinkedList {
      */
     public void append(int value) {
         if(this.root == null) {
-            this.root = new Node(value+1, null);
+            this.root = new Node(value, null);
             return;
         }
         // If it's just one element, add if after that one
         Node n = this.root;
         if(n.next == null) {
-            n.next = new Node(value+1, null);
+            n.next = new Node(value, null);
             return;
         }
         // Otherwise, loop until the end and add at the end with a null
         while(n.next != null) {
-            n = n.next;
+            n=n.next;
             n.next = new Node(value, null);
-            break;
+            n=n.next;
         }
     }
     /**
